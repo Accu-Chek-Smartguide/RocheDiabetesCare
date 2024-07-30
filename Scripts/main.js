3db48230b21583675316508e76d95a10022185e3
bruno silva costa <bruno.silvacosta.pro@gmail.com>
2024-07-30 11:26:06 +0200

    Fix bad repo

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,25 +1,7 @@
-window.onload = function() {
-    const logo = document.getElementById('logo');
-    const logoContainer = document.getElementById('logo-container');
-    const whiteScreen = document.getElementById('white-screen');
-    const arContainer = document.getElementById('ar-container');
+const splash = document.querySelector{.splash};
 
-    // Fade out the logo
-    setTimeout(() => {
-        logo.style.transition = 'opacity 5s';
-        logo.style.opacity = 0;
-    }, 1000);
-
-    // Fade out the white screen
-    setTimeout(() => {
-        logoContainer.style.display = 'none';
-        whiteScreen.style.transition = 'opacity 8s';
-        whiteScreen.style.opacity = 0;
-    }, 3000);
-
-    // Show the AR container
-    setTimeout(() => {
-        whiteScreen.style.display = 'none';
-        arContainer.style.display = 'block';
-    }, 5000);
-}
+document.addEventListener ("DOMContentLoaded", (e)=>{
+    setTimeout(()=>{
+        splash.classList.add('display-none');
+    }, 2000);
+})
