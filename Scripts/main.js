cdd4337a2e2209f245b6dc5f1c85b07d2ac70f17
bruno silva costa <bruno.silvacosta.pro@gmail.com>
2024-08-03 18:14:30 +0200

    Fix bad repo

--- Scripts/main.js
+++ Scripts/main.js
@@ -4,5 +4,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
         setTimeout(() => {
             document.getElementById('splash-screen').style.display = 'none';
         }, 1000); // Correspond à la durée de la transition (1 seconde)
-    }, 3000); // Durée du splash screen en millisecondes (ici 3 secondes)
+    }, 5000); // Durée du splash screen en millisecondes (ici 3 secondes)
 });
+const loader = new THREE.TextureLoader();
+const baseColorTexture = loader.load('https://github.com/BrunoSilvaCosta/RocheDiabetesCare/blob/main/Model/Box-MAT_BaseColor.jpg');
+const normalMapTexture = loader.load('https://github.com/BrunoSilvaCosta/RocheDiabetesCare/blob/main/Model/Box-MAT_Normal.jpg');
+const roughnessMapTexture = loader.load('https://github.com/BrunoSilvaCosta/RocheDiabetesCare/blob/main/Model/Box-MAT_Roughness.jpg');
+
+const material = new THREE.MeshStandardMaterial({
+  map: baseColorTexture,
+  normalMap: normalMapTexture,
+  roughnessMap: roughnessMapTexture
+});
+
+// Assurez-vous que ces textures sont visibles
+console.log(baseColorTexture, normalMapTexture, roughnessMapTexture);
