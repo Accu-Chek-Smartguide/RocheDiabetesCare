9b0e414a97543f008cf3974f30614e4e8aa62897
bruno silva costa <bruno.silvacosta.pro@gmail.com>
2024-08-07 15:05:16 +0200

    Fix bad repo

--- Scripts/main.js
+++ Scripts/main.js
@@ -3,9 +3,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
         document.getElementById('splash-screen').style.opacity = '0';
         setTimeout(() => {
             document.getElementById('splash-screen').style.display = 'none';
-            // Trigger the animation start event after splash screen is hidden
-            const animatedModel = document.querySelector('.animated-model');
-            animatedModel.emit('model-loaded');
         }, 1000); // Correspond à la durée de la transition (1 seconde)
-    }, 3000); // Durée du splash screen en millisecondes (ici 3 secondes)
+    }, 5000); // Durée du splash screen en millisecondes (ici 3 secondes)
 });
+const loader = new THREE.TextureLoader();
+const baseColorTexture = loader.load('Model/Box-MAT_BaseColor.jpg');
+const normalMapTexture = loader.load('Model/Box-MAT_Normal.jpg');
+const roughnessMapTexture = loader.load('Model/Box-MAT_Roughness.jpg');
+
+const material = new THREE.MeshStandardMaterial({
+  map: baseColorTexture,
+  normalMap: normalMapTexture,
+  roughnessMap: roughnessMapTexture
+});
+
+// Assurez-vous que ces textures sont visibles
+console.log(baseColorTexture, normalMapTexture, roughnessMapTexture);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
