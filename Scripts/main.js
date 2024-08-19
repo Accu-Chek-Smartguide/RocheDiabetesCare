9e4d6fb05293f63014dcd27031df6196fb4f4cc4
Accu-Check-SmartGuide <bruno.silvacosta.pro@gmail.com>
2024-08-19 18:10:50 +0200

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }, 1000); // Correspond à la durée de la transition (1 seconde)
     }, 5000); // Durée du splash screen en millisecondes (ici 3 secondes)
 });
+
 const loader = new THREE.TextureLoader();
 const baseColorTexture = loader.load('Model/Box-MAT_BaseColor.jpg');
 const normalMapTexture = loader.load('Model/Box-MAT_Normal.jpg');
@@ -20,6 +21,7 @@ const material = new THREE.MeshStandardMaterial({
 // Assurez-vous que ces textures sont visibles
 console.log(baseColorTexture, normalMapTexture, roughnessMapTexture);
 
-const renderer = new THREE.WebGLRenderer();
+// Initialize the renderer with logarithmicDepthBuffer set to true
+const renderer = new THREE.WebGLRenderer({ logarithmicDepthBuffer: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
